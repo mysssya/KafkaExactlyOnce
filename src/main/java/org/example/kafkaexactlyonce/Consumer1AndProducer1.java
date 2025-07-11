058f95195f596cb73457d87fb0455c5e324afd79
maryvito <maryv2@yandex.ru>
2025-07-11 13:03:41 +0300

    .

--- src/main/java/org/example/kafkaexactlyonce/Consumer1AndProducer1.java
+++ src/main/java/org/example/kafkaexactlyonce/Consumer1AndProducer1.java
@@ -21,32 +21,32 @@ public class Consumer1AndProducer1 {
         String groupId = "some-java-consumer" + UUID.randomUUID();
 
         Map<String, Object> prodConfig = new HashMap<>(producerConfig);
-//        prodConfig.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "ex");
+        prodConfig.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "ex");
         prodConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, HOST2);
 
         Map<String, Object> consConfig = new HashMap<>(consumerConfig);
-//        consConfig.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        consConfig.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         consConfig.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
 
         try (var consumer = new KafkaConsumer<String, String>(consConfig);
              var producer = new KafkaProducer<String, String>(prodConfig)) {
             consumer.subscribe(Arrays.asList("topic-1"));
-//            producer.initTransactions();
+            producer.initTransactions();
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, String> record : records) {
                     System.out.printf("offset = %d, key = %s, value = %s \n",
                             record.offset(), record.key(), record.value());
 
-//                    producer.beginTransaction();
+                    producer.beginTransaction();
                     producer.send(
                             new ProducerRecord<>("topic-2", record.key(), record.value() + "-processed"));
-//                    producer.sendOffsetsToTransaction(
-//                            Map.of(
-//                                    new TopicPartition(record.topic(), record.partition()),
-//                                    new OffsetAndMetadata(record.offset())),
-//                                    new ConsumerGroupMetadata(groupId));
-//                    producer.commitTransaction();
+                    producer.sendOffsetsToTransaction(
+                            Map.of(
+                                    new TopicPartition(record.topic(), record.partition()),
+                                    new OffsetAndMetadata(record.offset())),
+                                    new ConsumerGroupMetadata(groupId));
+                    producer.commitTransaction();
                 }
             }
         }
